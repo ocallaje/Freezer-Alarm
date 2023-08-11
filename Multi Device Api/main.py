a8d146dd48949d63c4fbdfd006c8c89310e1809a
ocallaje <ocallaje@tcd.ie>
2023-08-11 22:53:42 +0100

    requests

--- Multi Device Api/main.py
+++ Multi Device Api/main.py
@@ -1,8 +1,8 @@
 from email.mime.text import MIMEText
-import urllib.request
 import urllib.parse
 import json
 import send
+import requests
 
 freezeThresh = -14     # Freezer temperature threshold
 global trigger          # Variable accessible by thread functions
@@ -10,3 +10,10 @@ subject = "Freezer Alert!"
 body = "Lab Freezer Alert! Temperature of freezer has exceeded " + freezeThresh + "degrees! Please investigate"
 
 #API to call freezer data
+url = "192.168.0.100/data"     #mac of root node is 0C-DC-7E-57-A7-28
+payload = {}
+headers = {}
+response = requests.request("GET", url, headers=headers, data=payload)
+print(response.text)
+
+print("test")
